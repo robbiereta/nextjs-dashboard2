49fcad60388ce96d1b559e73df8c94b870a384bb
robbie reta <robbie.ret67@gmail.com>
2023-12-12 20:54:06 -0600

    5

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,4 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
